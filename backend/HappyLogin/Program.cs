5e2e9394dc4496bffc7d1a78469dca4c97c52a03
gisele-rosa <gisele.nl@hotmail.com>
2024-11-24 02:44:36 -0300

    added db context

--- backend/HappyLogin/Program.cs
+++ backend/HappyLogin/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.userSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
